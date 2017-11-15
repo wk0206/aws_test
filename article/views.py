e7161a802a9e59e52b389508ef38f257913638ee
wk0206 <wk0206@gmail.com>
2017-11-15 16:45:06 +0100

    blog

--- article/views.py
+++ article/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from article.models import Article
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse("test for parameter %s.")
+    post_list = Article.objects.all()
+    return render(request, 'home.html', {'post_list': post_list})
 
 def detail(request, parameter):
-    post = Article.objects.all()[int(parameter)]
-    str = ("title = %s, category = %s, date_time = %s, content = %s"
-           %(post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str)
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        return Http404
+    return render(request, 'post.html', {'post' : post})
+
+
+def test(request) :
+    return render(request, 'test.html', {'current_time': datetime.now()})
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -19,7 +19,9 @@ from article import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^$', views.home),
-    url(r'^(?P<parameter>\d+)/$', views.detail, name = 'detail'),
+    url(r'^$', views.home, name='home'),
+    url(r'^test/$', views.test),
+    #url(r'^(?P<parameter>\d+)/$', views.detail, name = 'detail'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 
 ]
